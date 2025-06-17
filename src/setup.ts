2efa7f4e027c6aff4f538f31b17576268fc40ac4
danielgstephenson <dgstephenson.econ@gmail.com>
2025-06-17 14:37:01 -0400

    update setup

--- src/setup.ts
+++ src/setup.ts
@@ -42,14 +42,15 @@ function getCardsInGame (state: State): Card[] {
   }
   const startMessage = `${startPlayer.name} started the game.`
   state.startingEpisode = state.history.addPublicChild(startMessage)
-  const ranks = range(0, 25)
-  const shuffleable = ranks.filter(i => i !== 5 && i !== 1)
+  const ranks = range(1, 25)
+  const notShuffleable = [1, 5, 8]
+  const shuffleable = ranks.filter(rank => !notShuffleable.includes(rank))
   const shuffled = shuffle(shuffleable, state.rand)
   const joinedRanks = arrayToString(shuffled)
-  const shuffleMessage = `Shuffled ${state.input.names.cards} 0, 2, 3, 4, and 6 through 25: ${joinedRanks}.`
+  const shuffleMessage = `Shuffled ${state.input.names.cards} 2, 3, 4, 6, 7, and 9 through 25: ${joinedRanks}.`
   state.startingEpisode.addPublicChild(shuffleMessage)
-  const dealCount = 12 + 2 * state.input.playerCount
-  const dealCountMessage = `This deal count is thirteen plus twice the number of players: ${dealCount}.`
+  const dealCount = 14 + state.input.playerCount
+  const dealCountMessage = `The deal count is fourteen plus the number of players: ${dealCount}.`
   state.startingEpisode.addPublicChild(dealCountMessage)
   const ranksInGame = shuffled.slice(0, dealCount)
   const ranksNotInGame = shuffled.slice(dealCount)
@@ -70,7 +71,7 @@ function getPortfolio (state: State, cardsInGame: Card[]): Card[] {
   const archiveCard = greenCards.shift()
   if (archiveCard == null) throw new Error('archiveCard is null')
   state.startingArchive = [archiveCard]
-  let archiveCardMessage = `The ${state.input.names.lowestRank} green ${state.input.names.card}, ${archiveCard.rank},`
+  let archiveCardMessage = `The lowest remaining green ${state.input.names.card}, ${archiveCard.rank},`
   archiveCardMessage += ` is ${state.input.names.archivedTo} the ${state.input.names.archive}.`
   state.startingEpisode.addPublicChild(archiveCardMessage)
   const redCards = cardsInGame.filter(card => card.color === 'Red')
@@ -82,7 +83,7 @@ function getPortfolio (state: State, cardsInGame: Card[]): Card[] {
   const portfolioGreen = getPortfolioColorCards(state, 'Green', greenCards)
   const portfolioRed = getPortfolioColorCards(state, 'Red', redCards)
   const portfolioYellow = getPortfolioColorCards(state, 'Yellow', yellowCards)
-  const portfolio = [new Card(5, state), ...portfolioGreen, ...portfolioRed, ...portfolioYellow]
+  const portfolio = [new Card(5, state), new Card(8, state), ...portfolioGreen, ...portfolioRed, ...portfolioYellow]
   const sortedPortfolio = Card.sortByRank(portfolio)
   const sortedPortfolioString = cardsToString(sortedPortfolio)
   const portfolioMessage = `The portfolio is: ${sortedPortfolioString}.`
@@ -93,21 +94,25 @@ function getPortfolio (state: State, cardsInGame: Card[]): Card[] {
 function getPortfolioColorCards (state: State, color: Color, colorCards: Card[]): Card[] {
   if (state.startingEpisode == null) throw new Error('startEpisode is null')
   const portfolioCounts = {
-    2: { Green: 2, Red: 3, Yellow: 2 },
-    3: { Green: 2, Red: 3, Yellow: 3 },
-    4: { Green: 3, Red: 3, Yellow: 3 },
-    5: { Green: 3, Red: 4, Yellow: 3 }
+    2: { Green: 1, Red: 4, Yellow: 3 },
+    3: { Green: 1, Red: 4, Yellow: 3 },
+    4: { Green: 1, Red: 4, Yellow: 3 },
+    5: { Green: 1, Red: 4, Yellow: 3 }
   }
   const portfolioCount = portfolioCounts[state.input.playerCount]
   const portfolioColorCards = colorCards.slice(0, portfolioCount[color])
   if (portfolioColorCards.length === 0) {
     const portfolioColorMessage = `There are no ${color.toLowerCase()} ${state.input.names.cards} remaining.`
     state.startingEpisode.addPublicChild(portfolioColorMessage)
+  } else if (portfolioCount[color] === 1) {
+    let portfolioColorMessage = `The lowest remaining ${color.toLowerCase()} `
+    portfolioColorMessage += `${state.input.names.card} is: ${portfolioColorCards[0].rank}.`
+    state.startingEpisode.addPublicChild(portfolioColorMessage)
   } else if (portfolioColorCards.length === 1) {
     const portfolioColorMessage = `The only remaining ${color.toLowerCase()} ${state.input.names.card} is ${portfolioColorCards[0].rank}`
     state.startingEpisode.addPublicChild(portfolioColorMessage)
   } else {
-    let portfolioColorMessage = `The ${numberToString(portfolioColorCards.length)} lowest ${color.toLowerCase()} `
+    let portfolioColorMessage = `The ${numberToString(portfolioColorCards.length)} lowest remaining ${color.toLowerCase()} `
     portfolioColorMessage += `${state.input.names.cards} are: ${cardsToString(portfolioColorCards)}.`
     state.startingEpisode.addPublicChild(portfolioColorMessage)
   }
